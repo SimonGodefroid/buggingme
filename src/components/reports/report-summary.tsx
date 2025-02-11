ce7077eda6ea4d24ddad89e8c9491c50d111ee33
Simon G <17337190+SimonGodefroid@users.noreply.github.com>
2025-02-11 17:47:10 +0400

    fix(image-on-report): fix broken image on report and report summary layout tweak

--- src/components/reports/report-summary.tsx
+++ src/components/reports/report-summary.tsx
@@ -24,30 +24,45 @@ export default function ReportSummary({ report }: { report: ReportWithTags }) {
   return (
     <Card
       isHoverable
-      className="w-full"
+      className="w-full gap-4 p-4 "
       key={report.id}
       isPressable
       onPress={() => router.push(`/reports/${report.id}`)}
     >
-      <CardHeader className="flex justify-around ">
-        <div className="w-48 h-32 flex-shrink-0">
-          <img
-            src={report.attachments?.[0]?.url || '/placeholder.png'}
-            alt={report.company?.name}
-            className="w-full h-full object-cover rounded-md"
-          />
+      <CardBody className="flex">
+        <div className="flex xs:flex-row">
+          <div className="flex flex-col items-center md:flex-row justify-between md:items-start w-full gap-10 xs:flex-wrap md:flex-nowrap 0">
+            {/* Left: Image */}
+            <div className="w-48 h-32 flex-shrink-0 ">
+              <img
+                src={
+                  report.attachments?.[0]?.url ||
+                  'https://placehold.co/200x200?text=No+attachment+yet'
+                }
+                alt={report.company?.name}
+                className="w-full h-full object-cover rounded-md"
+              />
+            </div>
+
+            {/* Middle: SiteBadge + Title (Now centered) */}
+            <div className="flex flex-col md:flex-row md:items-start flex-wrap xl:flex-nowrap w-full p-4 gap-4 tems-center sm:items-start ">
+              <div className="flex md:w-1/5 justify-center gap-4">
+                <SiteBadge company={report.company as Company} />
+              </div>
+              <div className="w-full md:w-4/5">
+                <p className="text-md text-center md:text-left w-full">
+                  {report.title}
+                </p>
+              </div>
+            </div>
+
+            {/* Right: Status + Category (Moves below image on small screens) */}
+            <div className="flex md:justify-start gap-4 md:flex-col h-full items-center ">
+              <Status status={report.status} />
+              <Category category={report.category} />
+            </div>
+          </div>
         </div>
-        <SiteBadge company={report.company as Company} />
-        <div className="flex gap-3">
-          <p className="text-md">{report.title}</p>
-        </div>
-        <div className="flex gap-4">
-          <Status status={report.status} />
-          <Category category={report.category} />
-        </div>
-      </CardHeader>
-      <CardBody>
-        {/* <p>Categories: {report.tags.map((tag) => tag.name).join(', ')}</p> */}
       </CardBody>
     </Card>
   );
